9fbfa3a371a9fee6934924326f492c4eb1d4dce1
nelio alves <brunopaesdemedeiros@gmail.com>
2023-01-13 21:47:02 -0300

    Simple consultation with query methods

--- src/main/java/com/brunodemedeiros/workshopmongo/services/PostService.java
+++ src/main/java/com/brunodemedeiros/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.brunodemedeiros.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,8 @@ public class PostService {
 		}
 		return post;
 	}
-
+	
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
